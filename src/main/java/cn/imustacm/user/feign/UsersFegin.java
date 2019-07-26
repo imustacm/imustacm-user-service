b4a550fb64a6a458ad05ad249150375fe00304a1
ChocoliBao <837104975@qq.com>
2019-07-26 09:49:16 +0800

    Add verification code method.

--- src/main/java/cn/imustacm/user/feign/UsersFegin.java
+++ src/main/java/cn/imustacm/user/feign/UsersFegin.java
@@ -3,8 +3,20 @@ package cn.imustacm.user.feign;
 import cn.imustacm.user.IUsersService;
 import cn.imustacm.user.model.Users;
 import cn.imustacm.user.service.UsersService;
+import cn.imustacm.user.utils.ImageCodeUtils;
+import cn.imustacm.user.utils.RedisUtils;
+import com.alibaba.fastjson.JSONObject;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 
 @RestController
@@ -12,16 +24,35 @@ public class UsersFegin implements IUsersService {
 
     @Autowired
     private UsersService usersService;
-
+    @Autowired
+    RedisTemplate<Object, Object> redisTemplate;
+    @Autowired
+    RedisConnectionFactory redisConnectionFactory;
 
     @Override
     public Boolean register() {
-
-        // 0 创建一个用户对象
-        Users user = Users.builder()
-                .username("test")
-                .gender(1)
-                .build();
+        Users user = Users.builder().username("test").gender(1).build();
         return usersService.save(user);
+    }
+
+    @Override
+    public JSONObject getImage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        redisTemplate = RedisUtils.redisTemplate(redisConnectionFactory);
+
+        String yy = this.getClass().getClassLoader().getResource("").getPath();
+        String path = yy + "/cn/imustacm/user/utils/new_words.txt";
+        JSONObject returnJson = ImageCodeUtils.getImage(path);
+        String word = returnJson.getString("word");
+        String base64Img = returnJson.getString("base64Img");
+
+        // 将验证码内容保存redis
+        String key = "Code:" + UUID.randomUUID().toString().replaceAll("-", "");
+        redisTemplate.opsForValue().set(key, word, 300, TimeUnit.SECONDS);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", key);
+        jsonObject.put("img", Base64.decode(base64Img));
+        return jsonObject;
     }
 }
