b1b36c246da9a89e513fe41cd064e1f365ab5159
ChocoliBao <837104975@qq.com>
2019-07-27 13:55:48 +0800

    Update verification code to dynamic.

--- src/main/java/cn/imustacm/user/feign/UsersFegin.java
+++ src/main/java/cn/imustacm/user/feign/UsersFegin.java
@@ -3,7 +3,8 @@ package cn.imustacm.user.feign;
 import cn.imustacm.user.IUsersService;
 import cn.imustacm.user.model.Users;
 import cn.imustacm.user.service.UsersService;
-import cn.imustacm.user.utils.ImageCodeUtils;
+import cn.imustacm.user.utils.Captcha;
+import cn.imustacm.user.utils.GifCaptcha;
 import cn.imustacm.user.utils.RedisUtils;
 import com.alibaba.fastjson.JSONObject;
 import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
@@ -12,9 +13,9 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ByteArrayOutputStream;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-
 
 @RestController
 public class UsersFegin implements IUsersService {
@@ -27,28 +28,42 @@ public class UsersFegin implements IUsersService {
     RedisConnectionFactory redisConnectionFactory;
 
     @Override
-    public Boolean register() {
-        Users user = Users.builder().username("test").gender(1).build();
-        return usersService.save(user);
+    public JSONObject register() {
+        Users user = Users.builder()
+                .username("test")
+                .password("123456")
+                .gender(1)
+                .visible(true)
+                .build();
+        boolean flag = usersService.save(user);
+
+        JSONObject jsonObject = new JSONObject();
+        if(flag) {
+            jsonObject.put("code", "0000");
+            jsonObject.put("msg", "注册成功！");
+        } else {
+            jsonObject.put("code", "0100");
+            jsonObject.put("msg", "注册失败：无法写入数据库！");
+        }
+        return jsonObject;
     }
 
     @Override
     public JSONObject getVerification() {
         redisTemplate = RedisUtils.redisTemplate(redisConnectionFactory);
-
-        String yy = this.getClass().getClassLoader().getResource("").getPath();
-        String path = yy + "/files/new_words.txt";
-        JSONObject returnJson = ImageCodeUtils.getImage(path);
-        String word = returnJson.getString("word");
-        String base64Img = returnJson.getString("base64Img");
-
-        // 将验证码内容保存redis
-        String key = "Code:" + UUID.randomUUID().toString().replaceAll("-", "");
+        Captcha captcha = new GifCaptcha(146,33,4);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        String base64 = captcha.out(bos);
+        String word = captcha.text().toLowerCase();
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        String key = "Code:" + uuid;
         redisTemplate.opsForValue().set(key, word, 300, TimeUnit.SECONDS);
 
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("code", key);
-        jsonObject.put("img", Base64.decode(base64Img));
+        jsonObject.put("code", "0000");
+        jsonObject.put("msg", "获取成功！");
+        jsonObject.put("key", key);
+        jsonObject.put("img", Base64.decode(base64));
         return jsonObject;
     }
 }
--- src/main/java/cn/imustacm/user/service/UsersService.java
+++ src/main/java/cn/imustacm/user/service/UsersService.java
@@ -4,9 +4,8 @@ import cn.imustacm.user.model.Users;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
- * <p>
+ *
  *  服务类
- * </p>
  *
  * @author liandong
  * @since 2019-07-21
