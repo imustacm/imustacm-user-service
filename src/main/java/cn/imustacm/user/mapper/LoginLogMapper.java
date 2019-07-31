5cfc4bb7de25a222987a35df77b4f279be23c44d
ChocoliBao <837104975@qq.com>
2019-07-31 18:25:10 +0800

    Add login method.

---
+++ src/main/java/cn/imustacm/user/mapper/LoginLogMapper.java
@@ -0,0 +1,17 @@
+package cn.imustacm.user.mapper;
+
+import cn.imustacm.user.model.LoginLog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ *
+ *  Mapper 接口
+ *
+ * @author wangjianli
+ * @since 2019-07-31
+ */
+@Mapper
+public interface LoginLogMapper extends BaseMapper<LoginLog> {
+
+}
---
+++ src/main/java/cn/imustacm/user/service/LoginLogService.java
@@ -0,0 +1,14 @@
+package cn.imustacm.user.service;
+
+import cn.imustacm.user.model.LoginLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * 服务类
+ *
+ * @author wangjianli
+ * @since 2019-07-31
+ */
+public interface LoginLogService extends IService<LoginLog> {
+
+}
