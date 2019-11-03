891befc18a3125a66f0f03430577ed1a33e61116
liandong <liandog@mmears.com>
2019-11-03 23:08:31 +0800

    update

---
+++ src/main/java/cn/imustacm/user/config/MyBatisPlusConfig.java
@@ -0,0 +1,15 @@
+package cn.imustacm.user.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        return page;
+    }
+}
